7237761c46464e735979c26ea11c526d25021c1c
Cameron Candelori <cameron.nigro@gmail.com>
2024-04-05 11:52:59 -0400

    Initial Commit

---
+++ InkNet/InkNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkNetApp.swift
+//  InkNet
+//
+//  Created by Cameron Candelori on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct InkNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
